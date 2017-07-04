6f3c89d232e3768e95e54525b329bcb9f870c3bc
Justin Samuels <blizm87@gmail.com>
2017-07-03 20:50:26 -0700

    basic express working

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path')
+
+const app = express();
+
+
+//CONFIG
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyparser.urlencoded({extended: true}));
+
+//ROUTES
+app.use('/index', require('./routes/index.js'));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("I am listening!!!");
+})
+
+
